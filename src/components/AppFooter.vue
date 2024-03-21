b23e257300bef2216b8cb7b93bc16efc46aabec1
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-21 18:22:50 +0100

    add footer component and footer partials

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+<template>
+    <footer class="site-footer">
+        <div class="container d-flex">
+
+            <section class="business">
+                <h4> Boolando s.r.l.</h4>
+                <ul class="business-info d-flex">
+                    <li>Informazioni legali</li>
+                    <li>Informativa sulla privacy</li>
+                    <li>Diritto di recesso</li>
+                </ul>
+            </section>
+
+            <div class="social">
+                <h6>Trovaci anche su</h6>
+                <div class="social-icons d-flex">
+                    <i class="fa-brands fa-square-twitter" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-square-facebook" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-square-instagram" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-pinterest" style="color: #ffffff;"></i>
+                    <i class="fa-brands fa-square-youtube" style="color: #ffffff;"></i>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<style></style>
