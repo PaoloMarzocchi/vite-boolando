c76161a51dc670e7c0f14e1acd3bda5ea23172bc
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-21 18:31:57 +0100

    add header elements dinamically

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,12 @@
 <script>
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            categories: ['Donna', 'Uomo', 'Bambino'],
+            icons: ['fa-regular fa-user', 'fa-regular fa-heart', 'fa-solid fa-lock']
+        }
+    }
 }
 </script>
 
@@ -9,17 +15,15 @@ export default {
 
         <nav class="container d-flex">
             <ul class="d-flex grow">
-                <li>Donna</li>
-                <li>Uomo</li>
-                <li>Bambini</li>
+                <li v-for='category in categories'>
+                    <a href="#">{{ category }}</a>
+                </li>
             </ul>
             <div class="logo-container">
                 <img src="/img/boolean-logo.png" class="logo" alt="Logo boolean">
             </div>
             <div class="header-icons d-flex grow">
-                <i class="fa-regular fa-user" style="color: #ffffff;"></i>
-                <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
-                <i class="fa-solid fa-lock" style="color: #ffffff;"></i>
+                <i :class="icon" style="color: #ffffff;" v-for="icon in icons"></i>
             </div>
         </nav>
 
