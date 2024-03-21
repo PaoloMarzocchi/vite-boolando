a30d6db7013ab02081383ad6fd79e015121e1453
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-21 17:18:59 +0100

    add header component, variables, common-utilities and header partials

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header class="site-header">
+
+        <nav class="container d-flex">
+            <ul class="d-flex grow">
+                <li>Donna</li>
+                <li>Uomo</li>
+                <li>Bambini</li>
+            </ul>
+            <div class="logo-container">
+                <img src="/img/boolean-logo.png" class="logo" alt="Logo boolean">
+            </div>
+            <div class="header-icons d-flex grow">
+                <i class="fa-regular fa-user" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
+                <i class="fa-solid fa-lock" style="color: #ffffff;"></i>
+            </div>
+        </nav>
+
+    </header>
+</template>
+
+<style></style>
