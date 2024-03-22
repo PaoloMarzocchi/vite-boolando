6c0aed76bdeb217507bab535ca4ab0bf8c392e8b
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-22 17:14:13 +0100

    add footer elements dinamically and edit footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,13 @@
 <script>
 export default {
     name: 'AppFooter',
+    data() {
+        return {
+            companyName: 'Boolando s.r.l.',
+            legalInfos: ['Informazioni legali', 'Informativa sulla privacy', 'Diritto di recesso'],
+            socialIcons: ["fa-brands fa-square-twitter", "fa-brands fa-square-facebook", "fa-brands fa-square-instagram", "fa-brands fa-pinterest", "fa-brands fa-square-youtube"]
+        }
+    }
 }
 </script>
 
@@ -8,23 +15,21 @@ export default {
     <footer class="site-footer">
         <div class="container d-flex">
 
-            <section class="business">
-                <h4> Boolando s.r.l.</h4>
-                <ul class="business-info d-flex">
-                    <li>Informazioni legali</li>
-                    <li>Informativa sulla privacy</li>
-                    <li>Diritto di recesso</li>
+            <section class="company">
+                <h4> {{ companyName }}</h4>
+                <ul class="company-info d-flex">
+                    <li v-for="info in legalInfos">
+                        <a href="#">{{ info }}</a>
+                    </li>
                 </ul>
             </section>
 
             <div class="social">
                 <h6>Trovaci anche su</h6>
                 <div class="social-icons d-flex">
-                    <i class="fa-brands fa-square-twitter" style="color: #ffffff;"></i>
-                    <i class="fa-brands fa-square-facebook" style="color: #ffffff;"></i>
-                    <i class="fa-brands fa-square-instagram" style="color: #ffffff;"></i>
-                    <i class="fa-brands fa-pinterest" style="color: #ffffff;"></i>
-                    <i class="fa-brands fa-square-youtube" style="color: #ffffff;"></i>
+                    <a href="#" v-for="icon in socialIcons">
+                        <i :class="icon" style="color: #ffffff;"></i>
+                    </a>
                 </div>
             </div>
         </div>
